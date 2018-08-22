6e9dec022bfa07ff8dca2bda8186e88a64d2d23c
uo-clife-work <uo@clife.work>
2018-08-22 13:05:44 +0900

    リファクタリング

--- ClfMods/ClfCommon/ClfRefactor.lua
+++ ClfMods/ClfCommon/ClfRefactor.lua
@@ -60,6 +60,14 @@ function ClfRefactor.initialize()
 end
 
 
+local CONST = {
+	honorColor    = { r = 163, g =  73, b = 164 },
+	lostItemColor = { r = 146, g = 245, b = 153 },
+}
+
+-- オブハン表示時に、オブジェクトのIDを保持するテーブル
+ClfRefactor.FieldObjectIds = {}
+
 -- Scavenger用アイテム判定に使うtable
 local ScvItemTypesHues = false
 
@@ -95,6 +103,8 @@ function ClfRefactor.createObjectHandles()
 		local WindowSetShowing = WindowSetShowing
 
 		-- variables
+		ClfRefactor.FieldObjectIds = {}
+		local FieldObjectIds = ClfRefactor.FieldObjectIds
 		local ObjectHandleWindow_ObjectsData_ObjectId = ObjectHandleWindow.ObjectsData.ObjectId
 		local ObjectHandleWindow_ObjectsData_Names = ObjectHandleWindow.ObjectsData.Names
 		local ObjectHandleWindow_ObjectsData_IsMobile = ObjectHandleWindow.ObjectsData.IsMobile
@@ -108,7 +118,8 @@ function ClfRefactor.createObjectHandles()
 		local CurrentHonor = Interface.CurrentHonor
 		local WindowData_ContainerWindow = WindowData.ContainerWindow
 		local WindowData_ContainerWindow_Type = WindowData_ContainerWindow.Type
-		local ContainerWindow_OpenContainers = ContainerWindow.OpenContainers
+		local WD_ItemProperties = WindowData.ItemProperties
+		local WD_ItemProperties_Type = WD_ItemProperties.Type
 
 		local grayColor = ObjectHandleWindow.grayColor
 		local TextColors = ObjectHandleWindow.TextColors
@@ -119,43 +130,41 @@ function ClfRefactor.createObjectHandles()
 		local isLostItemsOnly = ( objectHandleFilter == SSO.eLostItemsOnlyFilter )
 		local isItemsOrLostItemsFilter = isItemsOnly or isLostItemsOnly
 
-		-- define local function
-		local filterNames
-		local isNamePassedFilter = function( name )
-			return true
-		end
-		if ( type( ObjectHandleWindow_CurrentFilter ) == "wstring" and ObjectHandleWindow_CurrentFilter ~= L"" ) then
-			filterNames = {}
-			for cf in wstring.gmatch( ObjectHandleWindow_CurrentFilter, L"[^|]+" ) do
-				local cfFix = wLower( cf )
-				if ( cfFix ~= L"" and type( cfFix ) == "wstring" ) then
-					filterNames[ #filterNames + 1 ] = cfFix
-				end
-			end
-			if ( #filterNames ~= 0 ) then
-			isNamePassedFilter = function( name )
-				local nameFix = wLower( name )
-				if ( nameFix ~= L"" and type( nameFix ) == "wstring" ) then
-					for k = 1, #filterNames do
-						if ( wFind( nameFix, filterNames[ k ] ) ) then
-							return true
-						end
-					end
-				end
-				return false
-			end
-			end
-		end
-
 		-- define local variables
-		local honorColor    = { r = 163, g =  73, b = 164 }
-		local lostItemColor = { r = 146, g = 245, b = 153 }
 		local ignoreItemIds = {}
 		local ContainerWindow_IgnoreItems = ContainerWindow.IgnoreItems
 		for j = 1, #ContainerWindow_IgnoreItems do
 			local id = ContainerWindow_IgnoreItems[ j ] and ContainerWindow_IgnoreItems[ j ].id
 			if ( id ) then
 				ignoreItemIds[ id ] = true
+			end
+		end
+
+		-- 文字フィルターの配列
+		local filterStrings
+		-- 文字フィルターチェック用関数： 通常（文字フィルターが無い時）は必ず true を返す
+		local isNamePassedFilter = _return_true
+
+		if ( type( ObjectHandleWindow_CurrentFilter ) == "wstring" and ObjectHandleWindow_CurrentFilter ~= L"" ) then
+			filterStrings = {}
+			for cf in wstring.gmatch( ObjectHandleWindow_CurrentFilter, L"[^|]+" ) do
+				local cfFix = wLower( cf )
+				if ( cfFix ~= L"" and type( cfFix ) == "wstring" ) then
+					filterStrings[ #filterStrings + 1 ] = cfFix
+				end
+			end
+			if ( #filterStrings ~= 0 ) then
+				isNamePassedFilter = function( name )
+					if ( type( name ) == "wstring" ) then
+						local nameFix = wLower( name )
+						for k = 1, #filterStrings do
+							if ( wFind( nameFix, filterStrings[ k ] ) ) then
+								return true
+							end
+						end
+					end
+					return false
+				end
 			end
 		end
 
@@ -179,36 +188,39 @@ function ClfRefactor.createObjectHandles()
 			if ( not name or name == L"" or name == L"Treasure Sand" ) then
 				continue
 			end
-			if ( not isNamePassedFilter( name ) ) then
+			if ( isNamePassedFilter( name ) == false ) then
 				continue
 			end
 
 			local ObjectDataIsMobile = ObjectHandleWindow_ObjectsData_IsMobile[ i ]
+			if ( not ObjectDataIsMobile ) then
+				FieldObjectIds[ #FieldObjectIds + 1 ] = objectId
+			end
+
 			if ( isItemsOrLostItemsFilter ) then
 				if ( ObjectDataIsMobile ) then
 					continue
 				end
 
-				local removeOnComplete = false
 				local containerData = WindowData_ContainerWindow[ objectId ]
 				if ( not containerData ) then
 					RegisterWindowData( WindowData_ContainerWindow_Type, objectId )
 					containerData = WindowData_ContainerWindow[ objectId ]
-					removeOnComplete = true
 				end
 				if ( containerData and containerData.isCorpse ) then
 					continue
-				end
-				if ( removeOnComplete and ContainerWindow_OpenContainers[ objectId ] == nil ) then
-					UnregisterWindowData( WindowData_ContainerWindow_Type, objectId )
 				end
 			end
 
 			-- Lost Item check
 			local lostItem = false
 			if ( not ObjectDataIsMobile ) then
-				local tids = ItemProperties_GetObjectPropertiesArray( objectId, "Object Handle - check lost item" )
-				tids = tids and tids.PropertiesTids or {}
+				local propData = WD_ItemProperties[ objectId ]
+				if ( not propData ) then
+					RegisterWindowData( WD_ItemProperties_Type, objectId )
+					propData = WD_ItemProperties[ objectId ]
+				end
+				local tids = propData and propData.PropertiesTids or {}
 
 				for j = 1, #tids do
 					if ( tids[ j ] == 1151520 ) then
@@ -226,13 +238,13 @@ function ClfRefactor.createObjectHandles()
 			local hue
 			if ( ObjectDataIsMobile ) then
 				if ( objectId == CurrentHonor ) then
-					hue = honorColor
+					hue = CONST.honorColor
 				else
 					hue = TextColors[ ObjectHandleWindow_ObjectsData_Notoriety[ i ] + 1 ]
 				end
 			else
 				if ( lostItem ) then
-					hue = lostItemColor
+					hue = CONST.lostItemColor
 				else
 					hue = grayColor
 				end
@@ -276,12 +288,14 @@ function ClfRefactor.createObjectHandles()
 				local activeScavenger = Organizer.Scavenger[ Organizer_ActiveScavenger ]
 				for j = 1, activeScavengers_Items do
 					local itemL = activeScavenger[ j ]
-					local hues = scvItemTypesHues[ itemL.type ]
-					if ( hues ) then
-						hues[ #hues + 1 ] = itemL.hue
-					else
-						scvItemTypesHues[ itemL.type ] = { itemL.hue }
+					local itemL_type = itemL.type
+					if ( scvItemTypesHues[ itemL_type ] == nil ) then
+						scvItemTypesHues[ itemL_type ] = {}
 					end
+					scvItemTypesHues[ itemL_type ][ itemL.hue ] = true
+				end
+				if ( not next( ScvItemTypesHues ) ) then
+					ScvItemTypesHues = false
 				end
 			else
 				-- Scavenger用アイテムが登録されていない。tableを定義しない
@@ -324,16 +338,21 @@ function ClfRefactor.objectHandleWindowOnUpdate( timePassed )
 		local DoesPlayerHaveItem = DoesPlayerHaveItem
 		local ObjectHandle_DestroyObjectWindow = ObjectHandle.DestroyObjectWindow
 
-		for key, v in pairs( ObjectHandleWindow.hasWindow ) do
-			if ( not IsValidObject( key ) or DoesPlayerHaveItem( key ) ) then
-				ObjectHandle_DestroyObjectWindow( key )
+		for objectId in pairs( ObjectHandleWindow.hasWindow ) do
+			if ( not IsValidObject( objectId ) or DoesPlayerHaveItem( objectId ) ) then
+				ObjectHandle_DestroyObjectWindow( objectId )
 			end
 		end
 	end
 
 	-- ここから Scavengerの処理
-	if ( not ScvItemTypesHues ) then
-		-- アイテム判定用tableが定義されていない。終了
+	local SystemData_DragItem = SystemData.DragItem
+	if (
+			ScvItemTypesHues == false
+			or ContainerWindow.CanPickUp ~= true
+			or SystemData_DragItem.DragType == SystemData_DragItem.TYPE_ITEM
+		) then
+		-- アイテム判定用tableが定義されていない or アイテムをドラッグ出来る状態ではない： 終了
 		return
 	end
 
@@ -343,13 +362,13 @@ function ClfRefactor.objectHandleWindowOnUpdate( timePassed )
 	local RegisterWindowData = RegisterWindowData
 	local UnregisterWindowData = UnregisterWindowData
 
+	local ContainerWindow = ContainerWindow
 	local WindowData_ObjectInfo = WindowData.ObjectInfo
 	local ObjectInfo_Type = WindowData_ObjectInfo.Type
 
 	if ( #ScvObjectIds == 0 ) then
 		local IsMobile = IsMobile
 		local GetDistanceFromPlayer = GetDistanceFromPlayer
-		local isInArray
 
 		local scvItemTypesHues = ScvItemTypesHues
 		local WindowData_ContainerWindow
@@ -370,31 +389,25 @@ function ClfRefactor.objectHandleWindowOnUpdate( timePassed )
 				end
 				-- 死体かどうか判定
 				local containerData = WindowData_ContainerWindow[ objectId ]
-				local registCont = false
 				if ( not containerData ) then
 					RegisterWindowData( WindowData_ContainerWindow_Type, objectId )
 					containerData = WindowData_ContainerWindow[ objectId ]
-					registCont = true
 				end
 				if ( containerData and containerData.isCorpse ) then
 					-- 対象が死体だった。追加しない。
 					return false
-				elseif ( registCont == true and ContainerWindow_OpenContainers[ objectId ] == nil ) then
-					UnregisterWindowData( WindowData_ContainerWindow_Type, objectId )
 				end
 				ScvObjectIds[ #ScvObjectIds + 1 ] = objectId
 				return true
 			end
 		else
 			-- 通常のScavenger
-			isInArray = ClfUtil.isInArray
 			setScavengeItem = function( itemData, objectId )
 				if ( not itemData ) then
 					return false
 				end
 				-- Scavengerに登録してあるアイテムか判定
-				local hues = scvItemTypesHues[ itemData.objectType ]
-				if ( hues and isInArray( itemData.hueId, hues ) ) then
+				if ( scvItemTypesHues[ itemData.objectType ] and scvItemTypesHues[ itemData.objectType ][ itemData.hueId ] ) then
 					ScvObjectIds[ #ScvObjectIds + 1 ] = objectId
 					return true
 				end
@@ -402,50 +415,40 @@ function ClfRefactor.objectHandleWindowOnUpdate( timePassed )
 			end
 		end
 
-		for key, v in pairs( ObjectHandleWindow.hasWindow ) do
-			if ( IsMobile( key ) ) then
+		for objectId in pairs( ObjectHandleWindow.hasWindow ) do
+			if ( IsMobile( objectId ) ) then
 				continue
 			end
 
-			if ( IsValidObject( key ) and GetDistanceFromPlayer( key ) <= 2 ) then
-				local itemData = WindowData_ObjectInfo[ key ]
-				local registItem = false
+			if ( IsValidObject( objectId ) and GetDistanceFromPlayer( objectId ) <= 2 ) then
+				local itemData = WindowData_ObjectInfo[ objectId ]
 				if ( not itemData ) then
-					RegisterWindowData( ObjectInfo_Type, key )
-					itemData = WindowData_ObjectInfo[ key ]
-					registItem = true
+					RegisterWindowData( ObjectInfo_Type, objectId )
+					itemData = WindowData_ObjectInfo[ objectId ]
 				end
 
-				local added = setScavengeItem( itemData, key )
-
-				if ( registItem and not added ) then
-					UnregisterWindowData( ObjectInfo_Type, key )
-				end
+				setScavengeItem( itemData, objectId )
 			end
 		end
 	end
 
-	local ContainerWindow = ContainerWindow
-	if ( #ScvObjectIds > 0 and ContainerWindow.CanPickUp ) then
+	if ( #ScvObjectIds > 0 ) then
 		local OrganizeBag = Organizer.Scavengers_Cont[ Organizer.ActiveScavenger ]
 		if ( not OrganizeBag or OrganizeBag == 0 ) then
 			OrganizeBag = ContainerWindow.PlayerBackpack
 		end
 
-		local key, itemData, registItem
-		local pickUp = false
+		local objectId, itemData
 		local table_remove = table.remove
 		repeat
-			key = ScvObjectIds[ 1 ]
-			itemData = WindowData_ObjectInfo[ key ]
-			table_remove( ScvObjectIds, 1 )
-			registItem = false
+			local len = #ScvObjectIds
+			objectId = ScvObjectIds[ len ]
+			itemData = WindowData_ObjectInfo[ objectId ]
+			table_remove( ScvObjectIds, len )
 			if ( not itemData ) then
-				RegisterWindowData( ObjectInfo_Type, key )
-				itemData = WindowData_ObjectInfo[ key ]
-				registItem = true
+				RegisterWindowData( ObjectInfo_Type, objectId )
+				itemData = WindowData_ObjectInfo[ objectId ]
 				if ( not itemData ) then
-					UnregisterWindowData( ObjectInfo_Type, key )
 					if ( #ScvObjectIds == 0 ) then
 						ObjectHandleWindow.lastItem = 0
 						return
@@ -459,33 +462,28 @@ function ClfRefactor.objectHandleWindowOnUpdate( timePassed )
 		until ( itemData )
 
 		if ( itemData ) then
-			if ( key ~= ObjectHandleWindow.lastItem and itemData.containerId == 0 ) then
+			if ( objectId ~= ObjectHandleWindow.lastItem and itemData.containerId == 0 ) then
 				ContainerWindow.TimePassedSincePickUp = 0
 				ContainerWindow.CanPickUp = false
-				ObjectHandleWindow.lastItem = key
+				ObjectHandleWindow.lastItem = objectId
 
 				local dropContainerId
 				if ( itemData.objectType == 3821 and ClfSettings.EnableDropGoldToBP ) then
 					-- アイテムがゴールドの時は第一階層にドロップする
-					dropContainerId = MoveItemToContainer( key, itemData.quantity, ContainerWindow.PlayerBackpack )
+					dropContainerId = MoveItemToContainer( objectId, itemData.quantity, ContainerWindow.PlayerBackpack )
 				else
-					dropContainerId = MoveItemToContainer( key, itemData.quantity, OrganizeBag )
+					dropContainerId = MoveItemToContainer( objectId, itemData.quantity, OrganizeBag )
 				end
-				if ( not dropContainerId ) then
+				if ( dropContainerId == nil ) then
 					-- ドラッグ出来ないアイテムだった
 					ContainerWindow.CanPickUp = true
 				end
-				pickUp = true
 			end
 		end
 
 		if ( #ScvObjectIds == 0 ) then
 			ScvObjectIds = {}
 			ObjectHandleWindow.lastItem = 0
-		end
-
-		if ( registItem and not pickUp ) then
-			UnregisterWindowData( ObjectInfo_Type, key )
 		end
 	end
 end
@@ -504,6 +502,7 @@ function ClfRefactor.onVacuumStart()
 	OrganizeItemIds = false
 	local Actions = Actions
 	if ( Actions.MassOrganize == false ) then
+		Actions.VacuumObjects = Actions.VacuumObjects or {}
 		local Organizer = Organizer
 		local Organizer_ActiveOrganizer = Organizer.ActiveOrganizer
 		local activeOrganizerCont = Organizer.Organizers_Cont[ Organizer_ActiveOrganizer ]
@@ -522,13 +521,12 @@ function ClfRefactor.onVacuumStart()
 				local activeOrganizerItemNum = Organizer.Organizers_Items[ Organizer_ActiveOrganizer ]
 				for j = 1, activeOrganizerItemNum do
 					local itemL = activeOrganizerItemDatas[ j ]
-					if ( itemL.type > 0 ) then
-						local hues = organizeItemTypesHues[ itemL.type ]
-						if ( hues ) then
-							hues[ #hues + 1 ] = itemL.hue
-						else
-							organizeItemTypesHues[ itemL.type ] = { itemL.hue }
+					local itemL_type = itemL.type
+					if ( itemL_type > 0 ) then
+						if ( organizeItemTypesHues[ itemL_type ] == nil ) then
+							organizeItemTypesHues[ itemL_type ] = {}
 						end
+						organizeItemTypesHues[ itemL_type ][ itemL.hue ] = true
 					elseif ( itemL.id > 0 ) then
 						organizeItemIds[ itemL.id ] = true
 					end
@@ -540,6 +538,9 @@ function ClfRefactor.onVacuumStart()
 	else
 		Actions.MassOrganize = false
 		Actions.VacuumObjects = {}
+		if ( ClfSettings.EnableVacuumMsg ) then
+			WindowUtils.SendOverheadText( L"=== Stop Vacuum ===" , 46, true )
+		end
 	end
 end
 
@@ -575,17 +576,14 @@ function ClfRefactor.onMassOrganizer( timePassed )
 				local organizeItemIds = OrganizeItemIds
 
 				local DoesWindowExist = DoesWindowExist
-				local addExtOrganizerItem = ClfOrganizer and ClfOrganizer.organizerAddItem or function( objectId, itemData, containerId ) return false end
-				local isInArray = ClfUtil.isInArray
+				local addExtOrganizerItem = ClfOrganizer and ClfOrganizer.organizerAddItem or _return_false
 
 				-- コンテナ内のアイテムがルート対象ならテーブルに追加するメソッド
 				local setVacuumObject = function( itemData, objectId, containerId )
 					if ( not itemData ) then
 						return false
 					end
-
-					local hues = organizeItemTypesHues[ itemData.objectType ]
-					if ( hues and isInArray( itemData.hueId, hues ) ) then
+					if ( organizeItemTypesHues[ itemData.objectType ] and organizeItemTypesHues[ itemData.objectType ][ itemData.hueId ] ) then
 						Actions_VacuumObjects[ #Actions_VacuumObjects + 1 ] = objectId
 						return true
 					elseif ( organizeItemIds[ objectId ] == true ) then
@@ -605,13 +603,13 @@ function ClfRefactor.onMassOrganizer( timePassed )
 						end
 					end
 				else
-					closeContainerWin = function( containerId, addFromCont, containerData ) end
+					closeContainerWin = _void
 				end
 
 				local WindowData_ContainerWindow = WindowData.ContainerWindow
 
 				-- 全コンテナ内のアイテムチェック開始
-				for id, v in pairs( ContainerWindow.OpenContainers ) do
+				for id in pairs( ContainerWindow.OpenContainers ) do
 					if (
 							DoesWindowExist( "ContainerWindow_" .. id )
 							and id ~= ContainerWindow_PlayerBackpack
@@ -652,14 +650,14 @@ function ClfRefactor.onMassOrganizer( timePassed )
 			local objectId, itemData
 			local table_remove = table.remove
 			repeat
-				objectId = Actions_VacuumObjects[ 1 ]
+				local len = #Actions_VacuumObjects
+				objectId = Actions_VacuumObjects[ len ]
 				itemData = WindowData_ObjectInfo[ objectId ]
-				table_remove( Actions_VacuumObjects, 1 )
+				table_remove( Actions_VacuumObjects, len )
 				if ( not itemData ) then
 					RegisterWindowData( ObjectInfo_Type, objectId )
 					itemData = WindowData_ObjectInfo[ objectId ]
 					if ( not itemData ) then
-						UnregisterWindowData( ObjectInfo_Type, objectId )
 						if ( #Actions_VacuumObjects == 0 ) then
 							return
 						end
@@ -734,7 +732,7 @@ function ClfRefactor.onMassOrganizer( timePassed )
 				end
 			end
 
-			if ( not dropContainerId ) then
+			if ( dropContainerId == nil ) then
 				-- ドラッグ出来ないアイテムだった（コンテナから離れた、誰かがドラッグした、etc.）
 				ContainerWindow.CanPickUp = true
 				local str = L"Can't Drag: "
@@ -746,7 +744,6 @@ function ClfRefactor.onMassOrganizer( timePassed )
 				WindowUtils.ChatPrint( str, SystemData.ChatLogFilters.SYSTEM )
 				return
 			end
-
 		else
 			-- Vacuum対象のアイテムが無くなったので終了
 			Actions.MassOrganize = false
